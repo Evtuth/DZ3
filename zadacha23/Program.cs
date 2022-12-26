d17274c4938207edd7115d6c739c239bf46ce0c2
Vasiliy <evtuth@gmail.com>
2022-12-26 22:15:04 +0300

    added zadacha23 stepen 3

---
+++ zadacha23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23
+//Напишите программу, которая
+//принимает на вход
+//число (N)
+//и выдает таблицу кубов чисел от 1 до N.
+//3 -> 1 8 27
+//5 -> 1 8 27 64 125
+
+Console.Write("Введите любое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int x = 0;
+
+while (count <= N)
+{
+    x = count * count * count;
+    Console.Write(x + " ");
+    count++;
+}
